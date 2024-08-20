5fd171bb6c64c6231f8506f1dd237e02d72433cc
Jake <75503554+NahSahh@users.noreply.github.com>
2024-08-20 19:02:52 +0100

    refactor: descructive button & new variant

--- src/components/shadcn/button.tsx
+++ src/components/shadcn/button.tsx
@@ -5,20 +5,24 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "inline-flex items-center anim justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
+  "inline-flex items-center duration-300 active:translate-y-1 anim justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50",
   {
     variants: {
       variant: {
         default:
           "bg-button text-primary-foreground shadow hover:bg-button-hover",
         destructive:
-          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
+          "bg-red-600/30 border border-white/10 text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-white/10 bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
           "bg-secondary text-secondary-foreground shadow-sm hover:bg-secondary/80",
         ghost: "hover:bg-background-lighter hover:text-accent-foreground",
+        opaque:
+          "border border-white/10 bg-black/50 shadow-sm hover:bg-black/80 hover:text-accent-foreground",
+
         link: "text-primary underline-offset-4 hover:underline",
+
         gradient:
           "text-white bg-gradient-to-br from-primary-blue to-fuchsia-600",
       },
@@ -26,7 +30,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
-        icon: "h-9 w-9",
+        icon: "h-8 w-8",
         xl: "h-12 px-8",
       },
     },
